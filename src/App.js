3073e5e75d1d1665518a1b55f72b259ce4b4ce33
Kaur <kaurusai@gmail.com>
2019-02-05 11:25:03 +0200

    My first Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import "./App.css";
+import Counters from "./components/counters";
+/*
+  What did i learn:
+  Using Props
+  Raising and Handling Events
+  Lifting the State
+  Functional Components
+  Lifecycle Hooks
+*/
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 5 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  /*Initalize propertys in this class
+  constructor(props) {
+    super(props);
+    console.log("App - Constructor", this.props);
+  }
+  */
+
+  componentDidMount() {
+    //Ajax Calls or Axios
+    console.log("app Mounted");
+  }
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value --;
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Stateless Functional Compoent
+
+const NavBar = ({ totalCounters }) => {
+  console.log("NavBar- Rendered");
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
